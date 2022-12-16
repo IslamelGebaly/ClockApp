5053927258e34be75f47f5cb900338bd163218fc
IslamelGebaly <islamlang@gmail.com>
2022-12-16 21:24:35 +0200

    Minimum viable product

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,20 @@
-app = () => {
+function app() {
     const doc = document;
     const h1 = doc.createElement("h1");
     h1.textContent = "Welcome to my clock app!";
     doc.body.appendChild(h1);
 
     const h2 = doc.createElement("h2");
-    const date = new Date();
-    let time = date.getHours().toString() + ":" + date.getMinutes().toString() + ":" + date.getSeconds().toString();
-    h2.textContent = time
-    doc.body.appendChild(h2)
+    updateTime(doc, h2)
+}
+
+function updateTime(doc, element){
+    doc.body.appendChild(element)
+    const interval = setInterval(() => {
+        const date = new Date();
+        let time = date.toTimeString().split(" ")[0];
+        element.innerHTML = time;
+    }, 1000)
 }
 
 app()
